3b68c3a0283e7d637f904ff4e85371f01f2a1823
AshamanVK <ashaman.vk@gmail.com>
2016-01-12 21:58:09 +0300

    server without get-post and not full parser

---
+++ Get-Post.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2
+
+saved_data = {}
+
+
+def do_get():
+	
---
+++ Server.py
@@ -0,0 +1,40 @@
+#!usr/bin/python2
+
+import socket
+import Processor
+
+
+def main():
+    '''Server main loop'''
+
+    host = 'localhost'
+    port = 5000
+
+    try:
+        server_sock = socket.socket(socket.AF_INET,
+                                    socket.SOCK_STREAM,
+                                    socket.IPPROTO_TCP)
+        server_sock.setsockopt(socket.SOL_SOCKET,
+                               socket.SO_REUSEADDR, 1)
+        server_sock.bind((host, port))
+        server_sock.listen(1)
+        print 'Server start listen at:\n', str(host) + ':' + str(port)
+    except socket.error as msg:
+        print 'Oops! we have an error here...\n', msg
+        main()
+
+    while True:
+        client_sock, cliet_addr = server_sock.accept()
+
+        Processor.get_request_data(client_sock)
+        Processor.parse_data()
+
+        print '##STEP_5-----sending_response--------------'
+        print 'response to connection: HI! how are you?\n'
+        client_sock.send('HI! how are you?\n')
+
+        client_sock.close()
+
+
+if __name__ == '__main__':
+    main()
